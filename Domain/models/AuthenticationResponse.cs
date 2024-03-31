9a9f060892c48cc23c828362e9ac1610254b5c60
Kristifor Milchev <km@airzen.io>
2024-03-31 14:40:08 +0300

    Restoring deleted codebase

---
+++ Domain/models/AuthenticationResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public class AuthenticationResponse
+{
+    public required string Challenge { get; set; }
+    public required int Timeout { get; set; }
+    public required string RpId { get; set; }
+    public required string UserVerification { get; set; }
+    public Guid Id { get; set; }
+}
---
+++ Domain/models/StoredChallenges.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public class StoredChallenge
+{
+    public Guid Id { get; set; }
+    public string Challenge { get; set; }
+    public DateTime ExpiresAt { get; set; }
+    public Guid userId { get; set; }
+    public bool Verified { get; set; }
+}
