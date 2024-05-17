448f4bef70e1a5bcb05e8dfa998032ba56392ac6
Kristifor Milchev <km@airzen.io>
2024-05-17 13:33:10 +0300

    Adding the skeleton for the identities repository

--- repositories/identities.go
+++ repositories/identities.go
@@ -1,7 +1,33 @@
 package repositories
 
-import "leanmeal/api/interfaces"
+import (
+	"github.com/google/uuid"
+
+	"leanmeal/api/dtos"
+	"leanmeal/api/interfaces"
+	"leanmeal/api/models"
+)
 
 type IdentityRepository struct {
 	Storage interfaces.Storage
+}
+
+func (i *IdentityRepository) All() ([]models.Identity, error) {
+	return []models.Identity{}, nil
+}
+
+func (i *IdentityRepository) Get(id uuid.UUID) (models.Identity, error) {
+	return models.Identity{}, nil
+}
+
+func (i *IdentityRepository) GetKeyTypes() ([]models.KeyType, error) {
+	return []models.KeyType{}, nil
+}
+
+func (i *IdentityRepository) Add(identity dtos.CreateIdentity, keyData any) (uuid.UUID, error) {
+	return uuid.UUID{}, nil
+}
+
+func (i *IdentityRepository) Update(identity dtos.UpdateIdentity, keyData any) bool {
+	return true
 }
