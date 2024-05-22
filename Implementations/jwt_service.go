f2bde679006cb2ef25260adfc5bc49da5778d26b
Kristifor Milchev <km@airzen.io>
2024-05-22 23:18:51 +0300

    Extending the jwt_service to support mfa tokens.

--- Implementations/jwt_service.go
+++ Implementations/jwt_service.go
@@ -13,6 +13,26 @@ type JwtService struct {
 	Issuer string
 }
 
+func (JwtService *JwtService) IssueMfaToken(id string, deviceKey string) map[string]any {
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	expiresAt := time.Now().UTC().Add(time.Hour * 24 * 30).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":        id,
+		"nbf":       time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+		"exp":       expiresAt,
+		"iss":       JwtService.Issuer,
+		"deviceKey": deviceKey,
+		"role":      "mfa",
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(JwtService.Secret))
+
+	fmt.Println(tokenString, err)
+	return gin.H{"access_token": tokenString, "expires_at": expiresAt}
+}
+
 func (JwtService *JwtService) IssueToken(role string, id string, deviceKey string) map[string]any {
 	// Create a new token object, specifying signing method and the claims
 	// you would like it to contain.
@@ -23,6 +43,7 @@ func (JwtService *JwtService) IssueToken(role string, id string, deviceKey strin
 		"exp":       expiresAt,
 		"iss":       JwtService.Issuer,
 		"deviceKey": deviceKey,
+		"role":      role,
 	})
 
 	// Sign and get the complete encoded token as a string using the secret
@@ -33,6 +54,34 @@ func (JwtService *JwtService) IssueToken(role string, id string, deviceKey strin
 }
 
 func (JwtService *JwtService) ValidateToken(tokenString string) (bool, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(JwtService.Secret), nil
+	})
+	if err != nil {
+		return false, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		fmt.Println(claims["id"], claims["nbf"])
+
+		if claims["role"] == "mfa" {
+			return false, nil
+		}
+
+		return true, nil
+	} else {
+		fmt.Println(err)
+		return false, err
+	}
+}
+
+func (JwtService *JwtService) ValidateMfaToken(tokenString string) (bool, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
--- interfaces/jwt_service.go
+++ interfaces/jwt_service.go
@@ -1,7 +1,9 @@
 package interfaces
 
 type JwtService interface {
+	IssueMfaToken(id string, deviceKey string) map[string]any
 	IssueToken(role string, id string, deviceKey string) map[string]any
 	ValidateToken(token string) (bool, error)
+	ValidateMfaToken(token string) (bool, error)
 	ExtractValue(token string, key string) interface{}
 }
