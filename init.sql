b3714a8f0155f3704f08c8daec0b9016a07a2b0d
Kristifor Milchev <km@airzen.io>
2024-05-23 18:19:49 +0300

    Ensuring that seeds are ingored in the edge case where server initialization happens more than once.

--- init.sql
+++ init.sql
@@ -1,79 +1,79 @@
 -- Seed Event types
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (1,'Password Created');
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (2,'Password Updated');
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (3,'Password Deleted');
-INSERT INTO public.event_types(
-	id, name)
-	VALUES (4,'Secret Created');
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (5,'Secret Updated');
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (6, 'Secret Deleted');
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (7,'Identity Created');
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (8,'Identity Updated');
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (9,'Identity Deleted');
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (10,'Totp Created');
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (11,'Totp Updated');
-INSERT INTO public.event_types(
-	id,name)
-	VALUES (12,'Totp Deleted');
+INSERT INTO public.event_types(id, name)
+    VALUES (1, 'Password Created')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (2, 'Password Updated')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (3, 'Password Deleted')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (4, 'Secret Created')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (5, 'Secret Updated')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (6, 'Secret Deleted')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (7, 'Identity Created')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (8, 'Identity Updated')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (9, 'Identity Deleted')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (10, 'Totp Created')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (11, 'Totp Updated')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.event_types(id, name)
+    VALUES (12, 'Totp Deleted')
+    ON CONFLICT (id) DO NOTHING;
 
 -- Seed key types
+INSERT INTO public.key_types(id, name, description, has_size)
+    VALUES (1, 'ED25519', 'Edwards-curve Digital Signature Algorithm (EdDSA)', false)
+    ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO public.key_types(id, name, description, has_size)
-	VALUES (1, 'ED25519', 'Edwards-curve Digital Signature Algorithm (EdDSA)', false);
-
-INSERT INTO public.key_types(
-	id, name, description, has_size)
-	VALUES (2, 'RSA',  'Rivest–Shamir–Adleman (RSA)', true);
+    VALUES (2, 'RSA', 'Rivest–Shamir–Adleman (RSA)', true)
+    ON CONFLICT (id) DO NOTHING;
 
 -- Seed Time Based Code Types
-INSERT INTO public.time_based_code_types(
-	id, name)
-	VALUES (1, 'HOTP');
-INSERT INTO public.time_based_code_types(
-	id, name)
-	VALUES (2, 'TOTP');
-INSERT INTO public.time_based_code_types(
-	id, name)
-	VALUES (3, 'OTP');
+INSERT INTO public.time_based_code_types(id, name)
+    VALUES (1, 'HOTP')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.time_based_code_types(id, name)
+    VALUES (2, 'TOTP')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.time_based_code_types(id, name)
+    VALUES (3, 'OTP')
+    ON CONFLICT (id) DO NOTHING;
 
 -- Seed the algorithm table
-INSERT INTO public.time_based_algorithms(
-	id, name)
-	VALUES (1, 'AlgorithmSHA1');
-INSERT INTO public.time_based_algorithms(
-	id, name)
-	VALUES (2, 'AlgorithmSHA256');
-INSERT INTO public.time_based_algorithms(
-	id, name)
-	VALUES (3, 'AlgorithmSHA512');
-INSERT INTO public.time_based_algorithms(
-	id, name)
-	VALUES (4, 'AlgorithmMD5');
+INSERT INTO public.time_based_algorithms(id, name)
+    VALUES (1, 'AlgorithmSHA1')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.time_based_algorithms(id, name)
+    VALUES (2, 'AlgorithmSHA256')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.time_based_algorithms(id, name)
+    VALUES (3, 'AlgorithmSHA512')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.time_based_algorithms(id, name)
+    VALUES (4, 'AlgorithmMD5')
+    ON CONFLICT (id) DO NOTHING;
 
 -- Seed the mfa method types
-INSERT INTO public.mfa_method_types(
-	id, name)
-	VALUES (1, 'email');
-INSERT INTO public.mfa_method_types(
-	id, name)
-	VALUES (2, 'authenticator');
+INSERT INTO public.mfa_method_types(id, name)
+    VALUES (1, 'email')
+    ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.mfa_method_types(id, name)
+    VALUES (2, 'authenticator')
+    ON CONFLICT (id) DO NOTHING;
